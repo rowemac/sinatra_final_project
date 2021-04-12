498a1c437e25afdacb946fc4b6e09382acf8d047
Mac Rowe <m.rowe42@gmail.com>
2021-04-12 18:20:42 -0400

    Adds signup, login, session logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,58 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username].empty? || params[:password].empty?
+      redirect "/failure"
+
+    else 
+      user = User.new(username: params[:username], password: params[:password])
+      redirect "/albums"
+    end
+    
+  end
+
+  # get '/account' do
+  #   @user = User.find(session[:user_id])
+  #   erb :account
+  # end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "failure"
+    end 
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
   
 
 end
