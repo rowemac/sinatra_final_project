13409466d9914af650111e7c85c8b6351387b0a0
Mac Rowe <m.rowe42@gmail.com>
2021-04-12 18:01:58 -0400

    Seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,31 +3,31 @@ User.destroy_all
 Library.destroy_all
 
 
-50 times do
+50.times do
     Album.create(
-        title: Faker::Music.album
-        artist: Faker::Music.band
-        year: rand(1950..2021)
-        condition: ["New", "Good", "Fair", "Poor"].sample
-        reissue: [true, false].sample
-        genre: Faker::Music.genre
-        label: Faker::Fantasy::Tolkien.location + " Records"
-        producer: Faker::TvShows::TwinPeaks.character 
+        title: Faker::Music.album,
+        artist: Faker::Music.band,
+        year: rand(1950..2021),
+        condition: ["New", "Good", "Fair", "Poor"].sample,
+        reissue: [true, false].sample,
+        genre: Faker::Music.genre,
+        label: Faker::Fantasy::Tolkien.location + " Records",
+        producer: Faker::TvShows::TwinPeaks.character,
         single: Faker::Music::RockBand.song 
     )
 end 
 
-25 times do
+25.times do
     User.create(
-        username: Faker::Internet.username
-        password_digest: 
+        username: Faker::Internet.username,
+        password: "getinthereson", 
         email: Faker::Internet.email
     )
 end
 
-25 times do 
+25.times do 
     Library.create(
-        album_id: rand(1..50)
+        album_id: rand(1..50),
         user_id: rand(1..25)
     )
 end
