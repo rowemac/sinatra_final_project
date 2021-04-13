2bce5374711291dffbee5734e8fed0e610e0e09b
Mac Rowe <m.rowe42@gmail.com>
2021-04-13 14:14:47 -0400

    Fix patch method in UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/account"
+      redirect "/users/#{user.id}"
     else
       redirect "/failure"
     end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
 
-    get '/account' do
+    get '/users/:id' do
         @user = current_user
-        erb :'users/account'
+        erb :'users/show'
     end
 
-    get '/account/edit' do 
+    get '/users/:id/edit' do 
         @user = current_user
         erb :'users/edit'
     end 
+
+    patch '/users/:id' do
+        @user = current_user
+        @user.update(username: params[:username], email: params[:email])
+        redirect "/users/#{@user.id}"
+    end
 
 end 
