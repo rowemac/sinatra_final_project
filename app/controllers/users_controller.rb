cd81e21b9f6411972a10a279b5e3e016a9a57cdf
Mac Rowe <m.rowe42@gmail.com>
2021-04-13 11:50:50 -0400

    Updates users controller  get '/users' method to render all users with '/users/all'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,9 @@
 class UsersController < ApplicationController
-    # get '/signup' do
-    #     erb :signup
-    # end
-        
-    # post "/signup" do
-    #     user = User.new(username: params[:username], password: params[:password])
-    #     if user.params[:username].empty? || user.params[:password].empty?
-    #       redirect "/signup"
-    #     else 
-    #       redirect "/albums"
-    #     end
-    # end
+
+    get '/users' do
+        @users = User.all
+        erb :'/users/all'
+    end
+
 
 end 
