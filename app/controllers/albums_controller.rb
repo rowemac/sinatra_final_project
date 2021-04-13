bd1b1358616778c6f9de5a90dd98d0c183b0acd9
Mac Rowe <m.rowe42@gmail.com>
2021-04-13 08:44:32 -0400

    Updates erb file paths

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,32 +2,32 @@ class AlbumsController < ApplicationController
 
     get '/albums' do
         @albums = Album.all
-        erb :index
+        erb :'albums/index'
     end 
     
     get '/albums/new' do 
-        erb :new
+        erb :'albums/new'
     end
     
     post '/albums' do 
-        @album = Album.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
-        redirect "/albums/#{@Album.id}"
+        @album = Album.create(params)
+        redirect "/albums/#{@album.id}"
     end
     
     get '/albums/:id' do 
         @album = Album.find_by_id(params[:id])
-        erb :show
+        erb :'albums/show'
     end 
     
     get '/albums/:id/edit' do 
         @album = Album.find_by_id(params[:id])
-        erb :edit
+        erb :'albums/edit'
     end 
     
     patch '/albums/:id' do 
         @album = Album.find_by_id(params[:id])
         @album.update(params)
-        redirect to "/albums/#{@Album.id}"
+        redirect to "/albums/#{@album.id}"
     end
     
     delete '/albums/:id' do 
