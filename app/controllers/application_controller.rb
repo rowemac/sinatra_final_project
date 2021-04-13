8c3640b20fc656860bc24824ae2bd52bd644e9ed
Mac Rowe <m.rowe42@gmail.com>
2021-04-13 15:45:13 -0400

    Update application controller to have user login redirect to library. Comment out CRUD methods for library for tests. Update library index header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/users/#{user.id}"
+      redirect "/library"
     else
       redirect "/failure"
     end 
--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -4,38 +4,38 @@ class LibrariesController < ApplicationController
         erb :'libraries/index'
     end 
     
-    get '/library/new' do 
-        @user = current_user
-        erb :'libraries/new'
-    end
+    # get '/library/new' do 
+    #     @user = current_user
+    #     erb :'libraries/new'
+    # end
     
-    post '/library' do 
-        @library = Library.create(params)
-        redirect "/library/#{@library.id}"
-    end
+    # post '/library' do 
+    #     @library = Library.create(params)
+    #     redirect "/library/#{@library.id}"
+    # end
     
-    get '/library/:id' do 
-        @user = current_user
-        @library = @user.albums.find(params[:id])
-        erb :'libraries/show'
-    end 
+    # get '/library/:id' do 
+    #     @user = current_user
+    #     @library = @user.albums.find(params[:id])
+    #     erb :'libraries/show'
+    # end 
     
-    get '/library/:id/edit' do 
-        @user = current_user
-        @album = @user.albums.find(params[:id])
-        erb :'libraries/edit'
-    end 
+    # get '/library/:id/edit' do 
+    #     @user = current_user
+    #     @album = @user.albums.find(params[:id])
+    #     erb :'libraries/edit'
+    # end 
     
-    patch '/library/:id' do 
+    # patch '/library/:id' do 
         
-        @album = @user.albums.find(params[:id])
-        @album.update(params)
-        redirect to "/library/#{@album.id}"
-    end
+    #     @album = @user.albums.find(params[:id])
+    #     @album.update(params)
+    #     redirect to "/library/#{@album.id}"
+    # end
     
-    delete '/library/:id' do 
-        @album = Album.find_by_id(params[:id])
-        @album.delete
-        redirect to '/library'
-    end
+    # delete '/library/:id' do 
+    #     @album = Album.find_by_id(params[:id])
+    #     @album.delete
+    #     redirect to '/library'
+    # end
 end 
