b714565f0a2cd37aefdb3e23f76d6299f82416d4
Mac Rowe <m.rowe42@gmail.com>
2021-04-14 18:16:09 -0400

    Add dotenv and hid session key by adding a gitignore. Update Gemfile. Center text on views/welcome.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "vinylseshbaby"
+    set :session_secret, ENV['SECRET_KEY']
   end
 
   get "/" do
